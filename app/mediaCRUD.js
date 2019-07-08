339f9710cfc0ec64ed5a37eff87408314fecceaf
Daniel Peña Perez <dpena@paradigmadigital.com>
2019-07-08 08:13:49 +0200

    initiated Media Updload

---
+++ app/mediaCRUD.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var logger = require('./logger');
+var Response = require('./response');
+var BasicDAO = require('./basicMedia');
+let ObjectPath = require('object-path');
+
+const bucketImages = process.env.S3_bucket;
+
+
+const basicMedia = new BasicMedia({bucketDestination: bucketImages});
+
+
+exports.upload = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    let request = JSON.parse(event.body);
+    let base64String;
+    basicMedia.upload(request.data, function(err, data){
+        var response;
+        if(err){
+            response = Response.createResponse(500, err);
+        }else{
+            response = Response.createResponse(200, data);
+        }
+        callback(null, response);
+    });
+};
+
+
+
+
